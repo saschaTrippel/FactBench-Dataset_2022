6308f1a4fe9f2cba36a0a0613880f7ec09e22772
Sascha Trippel <strippel@mail.uni-paderborn.de>
2022-07-04 11:11:47 +0200

    replace script now adds to README

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Original Dataset <https://github.com/DeFacto/FactBench>
 Updated resources to fit DBpedia 2022
 
 # Updates
+* <http://dbpedia.org/resource/Kill_Bill> to <http://dbpedia.org/resource/Kill_Bill:_Volume_1>
--- replace.sh
+++ replace.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Run: ./replace.sh Cornelis_Janssens_van_Ceulen Cornelius_Johnson_(artist)
+set -e
 
 OLD=$1
 NEW=$2
@@ -12,3 +13,7 @@ for FILE in $FILES
 do
 	sed -i "s/$OLD/$NEW/g" $FILE
 done
+
+DBR="http://dbpedia.org/resource/"
+echo "* <$DBR$1> to <$DBR$2>" >> README.md
+
